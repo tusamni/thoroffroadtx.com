e43704eec2cf3ef364c27e7a4bf794ba6d166333
Erik Olsen <tusamni@gmail.com>
2022-08-15 21:39:50 -0500

    update colors and navigation

---
+++ src/data/business.ts
@@ -0,0 +1,14 @@
+export const Business = {
+    name_long: "THOR Off Road",
+    name_short: "THOR",
+    phone: "512-894-2790",
+    email: "sales@thoroffroadtx.com",
+    url: "https://thoroffroadtx.com",
+    address: {
+        street_01: "3996 US-290",
+        street_02: "Building B",
+        city: "Dripping Springs",
+        state: "TX",
+        zip: "78620"
+    }
+}

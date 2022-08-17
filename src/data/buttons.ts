0467d09e30ce329ca5ca7de1f3ee4f19db789634
Erik Olsen <erik@eriksolsen.com>
2022-08-16 22:47:30 -0500

    commit

--- src/data/buttons.ts
+++ src/data/buttons.ts
@@ -1,4 +1,4 @@
 export const Buttons = {
-    primary: ["bg-white", "hover:bg-neutral-50", "border-neutral-50", "hover:border-neutral-200", "shadow-tan-600/50"],
-    secondary: ["bg-yellow-500", "hover:bg-yellow-400", "border-yellow-400", "hover:border-yellow-500", "shadow-tan-600/50"],
+    primary: ["bg-white", "hover:bg-neutral-50", "border-neutral-400", "hover:border-neutral-500", "shadow-tan-600/50"],
+    secondary: ["bg-yellow-500", "hover:bg-yellow-400", "border-yellow-600", "hover:border-yellow-700/80", "shadow-tan-600/50"],
 }

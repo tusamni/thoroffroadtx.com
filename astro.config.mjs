d87b17d6d7716ad8e2abd70193bd2eef87a1d9e6
Erik Olsen <tusamni@gmail.com>
2022-08-24 20:58:32 -0500

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
@@ -8,9 +7,14 @@ export default defineConfig({
     integrations: [
         tailwind({
             config: {
-                applyBaseStyles: false
-            }
+                applyBaseStyles: false,
+            },
         }),
-        image()
-    ]
+        image(),
+    ],
+    vite: {
+        ssr: {
+            external: ["svgo"],
+        },
+    },
 });
--- src/functions/Media.js
+++ src/functions/Media.js
@@ -2,6 +2,7 @@ import * as path from 'path';
 
 // config
 import { imageConfig } from "@/src/config.ts"
+import devImage from "../../public/images/dev.jpg"
 
 // get the public image url
 export function getPublicPath(file) {
@@ -9,24 +10,37 @@ export function getPublicPath(file) {
 }
 
 export async function getImage(image) {
-    const publicPath = getPublicPath(image);
+    // load placeholder image and json when in dev mode
+    if (import.meta.env.MODE === "development") {
+        return {
+            "width": 1539,
+            "height": 2736,
+            "title": "THOR Off Road",
+            "description": "Carli lift kit, Rock Slide steps, Method wheels, Poison Spider bumper, Warn winch",
+            "alt": "Carli lift kit, Rock Slide steps, Method wheels, Poison Spider bumper, Warn winch",
+            "src": devImage.src,
+            "aspectRatio": 1539 / 2736
+        }
+    } else {
+        const publicPath = getPublicPath(image);
     
-    const extension = path.extname(publicPath); // extension of the image file
-    const baseFilename = path.basename(publicPath, extension); // filename of the image minus the extension
-    const dirPath = path.dirname(publicPath);
+        const extension = path.extname(publicPath); // extension of the image file
+        const baseFilename = path.basename(publicPath, extension); // filename of the image minus the extension
+        const dirPath = path.dirname(publicPath);
 
-    const metaFile = dirPath + '/' + baseFilename + '.json';
+        const metaFile = dirPath + '/' + baseFilename + '.json';
 
-    const response = await fetch(metaFile);
-    const json = await response.json();
+        const response = await fetch(metaFile);
+        const json = await response.json();
 
-    return {
-        "width": json.width,
-        "height": json.height,
-        "title": json.title,
-        "description": json.description,
-        "alt": json.alt,
-        "src": publicPath,
-        "aspectRatio": json.width / json.height
+        return {
+            "width": json.width,
+            "height": json.height,
+            "title": json.title,
+            "description": json.description,
+            "alt": json.alt,
+            "src": publicPath,
+            "aspectRatio": json.width / json.height
+        }
     }
 }

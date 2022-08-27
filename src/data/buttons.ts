dac514d8cd2595ca1b0b7868c3d75c420f4c4c4a
Erik Olsen <tusamni@gmail.com>
2022-08-26 20:39:15 -0500

    commit

--- src/data/buttons.ts
+++
@@ -1,4 +0,0 @@
-export const Buttons = {
-    primary: ["bg-white", "hover:bg-neutral-50", "border-neutral-400", "hover:border-neutral-500", "shadow-tan-600/50"],
-    secondary: ["bg-yellow-500", "hover:bg-yellow-400", "border-yellow-600", "hover:border-yellow-700/80", "shadow-tan-600/50"],
-}

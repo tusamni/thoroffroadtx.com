0d2b97eb42d274f7b69ddbcef54238fe21662c44
Erik Olsen <tusamni@gmail.com>
2022-08-31 17:28:29 -0500

    commit

--- src/data/menus.ts
+++ src/data/menus.ts
@@ -83,3 +83,48 @@ export const vehicles = [
         url: "/utv"
     }
 ]
+
+export const parts = [
+    {
+        name: "Bed & Roof",
+        description: "Expand the flexibility and cargo-carrying capability of your truck",
+        logo: "car-parts/door_outline",
+        url: "/parts/bed-roof"
+    },
+    {
+        name: "Body & Bumper",
+        description: "We have the highest quality bumpers, grill guards, fenders, side rails and more",
+        logo: "car-parts/pickup_outline",
+        url: "/parts/body-bumper"
+    },
+    {
+        name: "Lighting",
+        description: "High-performance LED headlight upgrades, pocket lights, light bars and more",
+        logo: "car-parts/lighting_outline",
+        url: "/parts/lighting"
+    },
+    {
+        name: "Performance",
+        description: "We sell and install proven engine, fuel delivery, exhaust and brake upgrades",
+        logo: "car-parts/tachometer_outline",
+        url: "/parts/performance"
+    },
+    {
+        name: "Specialties",
+        description: "Custom fuel tanks, gun racks, interior upgrades and custom fabrication",
+        logo: "car-parts/pistons_outline",
+        url: "/parts/specialties"
+    },
+    {
+        name: "Suspension",
+        description: "Get there fast, reliably, and in comfort with the perfect lift",
+        logo: "car-parts/coilover_outline",
+        url: "/parts/suspension"
+    },
+    {
+        name: "Wheel & Tire",
+        description: "We install performance wheel and tire combos, suited for your adventure",
+        logo: "car-parts/wheel_outline",
+        url: "/parts/wheel-tire"
+    }
+]

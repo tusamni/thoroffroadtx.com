b528cc14e1199fc30dc9c231ed2a41fc0d9dbb89
Erik Olsen <tusamni@gmail.com>
2022-09-06 17:50:08 -0500

    commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -65,5 +65,5 @@ module.exports = {
             },
         },
     },
-    plugins: [require("@tailwindcss/line-clamp")],
+    plugins: [require("@tailwindcss/line-clamp"), require("@tailwindcss/forms")],
 };

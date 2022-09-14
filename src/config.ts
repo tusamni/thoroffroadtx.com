f5f05201240ff12aef0b19d64891a1a8e1326e53
Erik Olsen <tusamni@gmail.com>
2022-09-14 17:15:44 -0500

    remove api key

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,3 @@
 export const imageConfig = {
     cloudUrl: "https://thoroffroad.nyc3.cdn.digitaloceanspaces.com",
 }
-
-export const apiKeys = {
-    maps: "AIzaSyCNEurErvvhqaP3E5HTzWm0rxIx1tarv_c"
-}

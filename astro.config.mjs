dba4f2793bf7ee1431b11471f0a6a35b28c22183
Erik Olsen <erik@eriksolsen.com>
2023-03-21 17:56:47 -0500

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        assets: true,
-    },
-    integrations: [
-        tailwind({
-            config: {
-                applyBaseStyles: false,
-            },
-        }),
-        alpinejs(),
-    ],
+  experimental: {
+    assets: true
+  },
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), alpinejs(), mdx()]
 });

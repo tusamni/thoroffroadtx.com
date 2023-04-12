f2a67bea35c70776def6c8baf8e0e5ecd6b47c4b
Erik Olsen <erik@eriksolsen.com>
2023-04-12 18:05:29 -0500

    commit

--- src/functions/Image.js
+++ src/functions/Image.js
@@ -1,3 +1,17 @@
+// config
+import { Image } from "../config.js";
+
+export function getImage(path) {
+    return `${Image.cloudUrl}/images/${path}`;
+}
+
+export function getBuildImage(id, number) {
+    // add a single leading zero
+    const filename = String(number).padStart(2, "0");
+
+    return `${Image.cloudUrl}/images/builds/${id}/${id}_${filename}.jpg`;
+}
+
 export function isPortrait(img) {
     var w = img.width,
         h = img.height;

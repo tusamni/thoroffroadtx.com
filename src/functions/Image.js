162417091912de13e5ea3ac1a93d0cbcd3e71a9c
Erik Olsen <erik@eriksolsen.com>
2023-04-25 15:40:16 -0500

    commit

--- src/functions/Image.js
+++ src/functions/Image.js
@@ -1,3 +1,9 @@
+import { Image } from "../config";
+
+export function getMedia(path) {
+    return `${Image.cloudUrl}/${path}`;
+}
+
 export function getBuildImage(path, number) {
     // add a single leading zero
     const filename = String(number).padStart(2, "0");

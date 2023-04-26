ee97f0471b45517a71e53fadd02bcac14c76927b
Erik Olsen <erik@eriksolsen.com>
2023-04-26 13:41:28 -0500

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
-
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
     experimental: {
         assets: true,
     },
+    site: "https://thoroffroadtx.com",
     integrations: [
         tailwind({
             config: {
@@ -17,5 +18,6 @@ export default defineConfig({
         }),
         alpinejs(),
         mdx(),
+        sitemap(),
     ],
 });

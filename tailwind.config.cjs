82ffb2c3f2aab237fd7a2862f1ced00a9d8534ce
Erik Olsen <tusamni@gmail.com>
2023-12-01 20:26:51 -0600

    commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,15 @@ module.exports = {
                 primary: "#EF233C",
             },
         },
+        letterSpacing: {
+            tightest: "-.85em",
+            tighter: "-.075em",
+            tight: "-.05em",
+            normal: "-.025em",
+            wide: "0",
+            wider: ".025em",
+            widest: ".05em",
+        },
     },
     plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography"), require("@tailwindcss/container-queries")],
 };

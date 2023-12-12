9f85e555311a71ace5171962f54bf59cd5bede9d
Erik Olsen <erik@eriksolsen.com>
2023-12-12 11:11:15 -0600

    commit

--- src/functions/Content.js
+++ src/functions/Content.js
@@ -1,1 +1,10 @@
+import { getEntry } from "astro:content";
 
+export async function getBuildTitle(id) {
+    const build = await getEntry("builds", id);
+
+    const make = await getEntry("makes", build.data.make.id);
+    const model = await getEntry("models", build.data.model.id);
+
+    return `${build.data.year} ${make.data.title} ${model.data.title} ${build.data.trim}`
+}

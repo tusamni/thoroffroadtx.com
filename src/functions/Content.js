a280018beedfa2a60d6987c5fa81fb03cd23017c
Erik Olsen <tusamni@gmail.com>
2023-12-11 21:48:47 -0600

    commit

--- src/functions/Content.js
+++ src/functions/Content.js
@@ -1,8 +1,1 @@
-import { getEntry, getEntries } from "astro:content";
 
-export async function getModels(make) {
-    const makeData = await getEntry("makes", make);
-    const models = await getEntries(makeData.data.models);
-
-    return models;
-}

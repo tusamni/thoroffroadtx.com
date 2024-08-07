f53a552ba1119b58f4b6e72b3ed0c129c0874ec7
erik <erik@eriksolsen.com>
2024-08-07 18:50:27 +0000

    redo getModels by slug and title

---
+++ public/scripts/builds.js
@@ -0,0 +1,36 @@
+function getModelsFromMake(allModels, formMake, formModel, includeOther = true, slug = false) {
+	// if includeOther, add it to the makes field
+	includeOther && formMake.add(new Option("Other", "Other"));
+
+	formMake.addEventListener(
+		"change",
+		function () {
+			if (this.value == "Other") {
+				clearField(formModel);
+
+				formModel.add(new Option("Other", "Other"));
+			} else {
+				clearField(formModel);
+
+				// map through available models
+				allModels.map((i) => {
+					if (i.data.make.id.includes(this.value.toLowerCase())) {
+						let newOption = new Option(i.data.title, slug ? i.data.slug : i.data.title);
+						formModel.add(newOption);
+					}
+				});
+
+				formModel.add(new Option("Other", "Other"));
+				formModel.selectedIndex = 0;
+			}
+		},
+		false,
+	);
+}
+
+// clear a select box
+function clearField(field) {
+	while (field.options.length > 0) {
+		field.remove(0);
+	}
+}

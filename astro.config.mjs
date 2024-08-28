1985cd64678cd059b52f531d63256170d51f0f36
erik <erik@eriksolsen.com>
2024-08-28 22:56:02 +0000

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import alpine from "@astrojs/alpinejs";
 import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://thoroffroadtx.com",
 	integrations: [alpine(), sitemap(), tailwind({
 		applyBaseStyles: true
-	}), mdx()],
+	})],
 	output: "hybrid",
 	adapter: netlify()
 });

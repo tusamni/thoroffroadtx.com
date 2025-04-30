1cd29b5157e1445c2957fdbafab66be0a2cb5819
erik <erik@eriksolsen.com>
2025-04-30 22:36:40 +0000

    update astrp

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from "astro/config";
 import alpine from "@astrojs/alpinejs";
 import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://thoroffroadtx.com",
-	integrations: [alpine(), sitemap(), tailwind({
-		applyBaseStyles: true
-	})],
-	adapter: netlify()
+	integrations: [alpine(), sitemap()],
+	adapter: netlify(),
+	vite: {
+		plugins: [tailwindcss()]
+	}
 });
